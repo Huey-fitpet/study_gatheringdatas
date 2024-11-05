4ec1811e463bfbcc9312a4eadaf6cacdb8f313fb
Huey <demonic0319@gmail.com>
2024-11-05 16:15:51 +0900

    feat : sentiment update

--- codes/APIs/insert_recode_in_mongo.py
+++ codes/APIs/insert_recode_in_mongo.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 
 class connect_mongo:
     def insert_recode_in_mongo(client, dbname, collectionname, input_list):
@@ -12,6 +12,15 @@ class connect_mongo:
         # results = collection.insert_many(input_list)
         # data frame insert 시 to_dict 해야함. 아니면 아래 에러가 남
         # The truth value of a DataFrame is ambiguous. Use a.empty, a.bool(), a.item(), a.any() or a.all().
-        results = collection.insert_many(input_list.to_dict(orient='records'))
+        # results = collection.insert_many(input_list.to_dict(orient='records'))
+
+        if isinstance(input_list, pd.DataFrame): # DataFrame인 경우
+            results = collection.insert_many(input_list.to_dict(orient='records'))
+        elif isinstance(input_list, list): # 리스트인 경우
+            results = collection.insert_many(input_list)
+        elif isinstance(input_list, dict): # 딕셔너리인 경우
+            results = collection.insert_one(input_list)
+        else:
+            print("error")
 
         return results
